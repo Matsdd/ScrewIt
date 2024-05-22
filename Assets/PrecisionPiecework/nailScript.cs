cee51ba27d84cc72b665487766e954a6cd53cdfe
RikWijtman <1045768@hr.nl>
2024-05-22 12:45:34 +0200

    nails

--- Assets/PrecisionPiecework/nailScript.cs
+++ Assets/PrecisionPiecework/nailScript.cs
@@ -4,24 +4,65 @@ using UnityEngine;
 
 public class nailScript : MonoBehaviour
 {
+    private bool nailHit = false;
+    public GameObject newNail;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("HitBox1"))
         {
-            Debug.Log("3 Points");
+            if (!nailHit)
+            {
+                Debug.Log("3 Points");
+                nailHit = true;
+
+                setHit();
+            }
         }
         else if (collision.gameObject.CompareTag("HitBox2"))
         {
-            Debug.Log("2 Points");
+            if (!nailHit)
+            {
+                Debug.Log("2 Points");
+                nailHit = true;
+
+                setHit();
+            }
         }
         else if (collision.gameObject.CompareTag("HitBox3"))
         {
-            Debug.Log("1 Point");
+            if (!nailHit)
+            {
+                Debug.Log("1 Point");
+                nailHit = true;
+
+                setHit();
+            }
         }
         else if (collision.gameObject.CompareTag("Hammer"))
         {
-            Debug.Log("Miss");
+            if (!nailHit)
+            {
+                Debug.Log("Miss");
+                nailHit = true;
+
+                setHit();
+            }
         }
+    }
+
+    void setHit()
+    {
+        Vector2 newPosition = this.gameObject.transform.position;
+        newPosition.y -= 20;
+        this.gameObject.transform.position = newPosition;
+
+        spawnNail();
+    }
+
+    void spawnNail()
+    {
+        Vector3 newPos = new Vector3(Random.Range(50, 700),30,0);
+        Instantiate(newNail,newPos,new Quaternion(0,0,0,0));
     }
 
 }
